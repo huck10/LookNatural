c1b1c8385b2e8e96f2c274c5593105b1e559bc56
karlb10 <karlbalaton10@gmail.com>
2026-05-31 20:49:00 +0800

    gg
    gg

---
+++ LookNatural/Assets/NetcodePlus/Demo/Scripts/Tools/WebGLTool.cs
@@ -0,0 +1,14 @@
+﻿
+namespace NetcodePlus
+{
+    public class WebGLTool
+    {
+
+        public static bool IsMobile()
+        {
+            return UnityEngine.Device.Application.isMobilePlatform;
+        }
+
+    }
+
+}
